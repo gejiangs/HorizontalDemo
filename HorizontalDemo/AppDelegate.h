92f06e082568cb11ab651fe439bf6ac2b9e60cc6
gejiangs <664553782@qq.com>
2019-03-22 09:19:26 +0800

    Initial Commit

---
+++ HorizontalDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  HorizontalDemo
+//
+//  Created by gejiangs on 2019/3/22.
+//  Copyright © 2019 gejiangs. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
